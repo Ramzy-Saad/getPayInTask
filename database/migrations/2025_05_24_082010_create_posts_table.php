1220d33d002cce32eca949cda7ea80c548d906dc
Ramzy Saad <www.ramzysaad16@gmail.com>
2025-05-24 17:49:24 +0300

    update type of image_url

--- database/migrations/2025_05_24_082010_create_posts_table.php
+++ database/migrations/2025_05_24_082010_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('content');
-            $table->string('image_url')->nullable();
+            $table->longText('image_url')->nullable();
             $table->timestamp('scheduled_time')->nullable();
             $table->enum('status', ['draft', 'scheduled', 'published']);
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
